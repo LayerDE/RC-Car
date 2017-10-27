5caf99c4a412dd6c71eeb04eca3f833b7d04ee60
LayerDE <the_gamer@gmx.net>
2017-10-27 16:53:49 +0200

    looks better now

--- RC-Car/main.c
+++ RC-Car/main.c
@@ -5,36 +5,33 @@
  * Author : Victor
  * 
  */ 
-//Interner Takt 3.3V
-#define BIT(n) (1<<n)
-#define CONFIG_BYTE(byte,on,off) byte = ((byte & (0xFF^(off)))|on)
+// Interner Takt 8Mhz@3.3V
 #define F_CPU 8000000UL
 
-#include <avr/io.h>//for Pin IO
-#include <avr/interrupt.h>//for Interrupts
-#include <avr/sleep.h>//for IDLE mode
-#include <avr/eeprom.h>//for programming values
+#define BIT(n) (1<<n)
+#define CONFIG_BYTE(byte,on,off) byte = ((byte & (0xFF^(off)))|on)
+
+#include <avr/io.h>			// for Pin IO
+#include <avr/interrupt.h>	// for Interrupts
+#include <avr/sleep.h>		// for IDLE mode
+#include <avr/eeprom.h>		// for programming values
 #include "pinout_car.h"
 #include "sx1278_defs.h"
 
 #define schedule_max (1<<4)
-//schedule_max-1 tasks work without any bugs  schedule_max must be 2^n for the performance (if its not 2^n its a performance issue)
-#define schedule_inc(x) (x=(x+1)&(schedule_max-1))
-//#define schedule_inc(x) ((x+1)>schedule_max?(++x):(x=0))
+// schedule_max-1 tasks work without any bugs  schedule_max must be 2^n for the performance (if its not 2^n its a performance issue)
+#define schedule_inc(x) (x = (x + 1) & (schedule_max - 1))
+// #define schedule_inc(x) ((x + 1) > schedule_max ? (++x) : (x=0))
 #define default_schedule sleep
-
-#define HEADERSIZE 0
 
 typedef void (*task)(void);
 char cData;
-unsigned char schedule_first=0;
-unsigned char schedule_last=schedule_max-1;
+unsigned char schedule_first = 0;
+unsigned char schedule_last = schedule_max - 1;
 task scheduler[schedule_max];
-task ADC_task;//reserved
-unsigned char spi_buffer[2][4];
-unsigned char spi_buffer_index;
+task ADC_task; // reserved
 
-//Car
+// Car
 void exec_command(unsigned char command){
 	switch(command){
 		case 0:
@@ -42,40 +39,31 @@ void exec_command(unsigned char command){
 	}
 }
 
-// utterly pointless separate 'functions'
-#define SPI_write(c)    spi(c)
-#define SPI_read(c)     spi(0)
-
-uint8_t spi(uint8_t c)
-{
-	SPDR = c;
-	while (!(SPSR & (1 << SPIF)));
-	return SPDR;
-}
-
 //tasks
 void sleep(){
-	set_sleep_mode(SLEEP_MODE_IDLE);//detup idle no wakeup time only 1/4 powerconsumption
+	set_sleep_mode(SLEEP_MODE_IDLE);//setup IDLE mode. only 1/4 powerconsumption
 	sleep_mode();//goto sleep
 }
 
-void add_task(task schedule) {//add task to scheduler
+void add_task(task schedule) {// add task to scheduler
 	scheduler[schedule_inc(schedule_last)]=schedule;
 }
 
+/*
 void get_lora_package(){//reserved later car func
 	unsigned char pack_size=SPI_read();
 	for(unsigned char i=0;i<HEADERSIZE;i++) spi_buffer[!spi_buffer_index][i]=SPI_read();
 	spi_buffer_index=!spi_buffer_index;
 	for(unsigned char i=0;i<pack_size;i++) exec_command(SPI_read());
 }
-
+*/
 #define SERVO_COUNT 4
 #define PRESCALER 1
 #define SERVO_PERIODE (unsigned int)20000/4*8/PRESCALER
 #define SERVO_MIDDLE 1500*8/PRESCALER
 #define SERVO_CALC(int16) (SERVO_MIDDLE*8/PRESCALER+int16)
-//declare an eeprom array
+// declare an eeprom array
+unsigned char servo_index = 0; // index of the servo who gets signal now
 unsigned int EEMEM servo_mid_eeprom[SERVO_COUNT]={1500*8/PRESCALER,1500*8/PRESCALER,1500*8/PRESCALER,1500*8/PRESCALER};
 unsigned int EEMEM servo_min_eeprom[SERVO_COUNT]={1000*8/PRESCALER,1000*8/PRESCALER,1000*8/PRESCALER,1000*8/PRESCALER};
 unsigned int EEMEM servo_max_eeprom[SERVO_COUNT]={2000*8/PRESCALER,2000*8/PRESCALER,2000*8/PRESCALER,2000*8/PRESCALER};
@@ -88,23 +76,23 @@ unsigned int servo_max_ram[SERVO_COUNT];
 unsigned char spi_buffer_ptr=0;
 unsigned char spi_buffer[SPI_BUFFER_SIZE]
 void init_servos(){
-	//direction
-	//WGM1 to mode 4 for clear on compare with OCR1A
+	// direction
+	// WGM1 to mode 4 for clear on compare with OCR1A
 	CONFIG_BYTE(TCCR1A , BIT(COM1A1) | BIT(COM1B1) , BIT(WGM10) | BIT(WGM11));// FastPWM Mode mode TOP determined by ICR1 - non-inverting Compare Output mode
 	CONFIG_BYTE(TCCR1B , BIT(WGM12) | BIT(CS10) , BIT(CS11) | BIT(CS12) | BIT(WGM13));    // set prescaler to 1, FastPWM Mode mode continued
 	CONFIG_BYTE(TIFR0 , BIT(OCF1A) | BIT(OCF1B),0);
 	//ICR1 = 20000;      // set period to 20 ms
 	OCR1A = SERVO_PERIODE;      // set count to 1500 us - 90 degree
-	OCR1B = 1500*8;      // set count to 1500 us - 90 degree
+	OCR1B = servo_mid_eeprom[servo_index];      // set count to 1500 us - 90 degree
 	TCNT1 = 0;         // reset timer
 }
 
 void init_spi_lora()
 {
-	/* Set MOSI and SCK output, all others input */
-	DDRB = (1<<DDB3)|(1<<DDB5);
-	/* Enable SPI, Master, set clock rate fck/16 */
-	SPCR = (1<<SPE)|(1<<MSTR)|(1<<SPR0);
+	// Set MOSI and SCK output, all others input
+	DDRB = (1 << DDB3) | (1 << DDB5);
+	// Enable SPI, Master, set clock rate fck/16
+	SPCR = (1 << SPE) | (1 << MSTR ) | (1 << SPR0);
 	
 	//INT0 for pack reciving
 	EIMSK |= (1 << INT0);     // Turns on INT0
@@ -118,18 +106,18 @@ void init(){//first task to execute
 	add_task(init_servos);
 }
 
-//teil der uhr
+// teil der uhr
 #define CLK_PRECOUNT (0x0100-200)
 unsigned char precount=0;
 unsigned int seconds=0;
-void clock_inc(){//kleine ungenaue uhr die mitläuft
+void clock_inc(){ // kleine ungenaue uhr die mitläuft
 	if(++precount) return;
 	precount=CLK_PRECOUNT;
 	seconds++;
 }
 
-//end tasks
-ISR(ADC_vect)//ADC isr reserved
+// end tasks
+ISR(ADC_vect)// ADC isr reserved
 {
 	/*for using the ISR
 	ADMUX = (1<<REFS0);               // Set Reference to AVCC and input to ADC0
@@ -139,7 +127,7 @@ ISR(ADC_vect)//ADC isr reserved
 	add_task(ADC_task);
 }
 
-ISR(INT0_vect)//lora pack reciving
+ISR(INT0_vect) // lora pack reciving
 {
 	/*
 		low
@@ -155,53 +143,19 @@ ISR(INT0_vect)//lora pack reciving
 	}
 	*/
 	
-	/* interrupt code here */
 	add_task(get_lora_package);
 }
-/*
-#define BUFSIZE 20
-volatile unsigned char incoming[BUFSIZE];
-volatile short int received=0;
 
-void send_message()
-{
-	setup_spi(SPI_MODE_1, SPI_MSB, SPI_NO_INTERRUPT, SPI_MSTR_CLK8);
-	if (SPCR & (1<<MSTR)) { // if we are still in master mode
-		SELECT_OTHER; // tell other device to flash LED twice
-		send_spi(FLASH_LED_COMMAND); send_spi(0x02); send_spi(0x00);
-		DESELECT_OTHER;
-	}
-	setup_spi(SPI_MODE_1, SPI_MSB, SPI_INTERRUPT, SPI_SLAVE);
-}
-
-void parse_message()
-{
-	switch(incoming[0]) {
-		case FLASH_LED_COMMAND:
-		flash_led(incoming[1]);
-		break;
-		default:
-		flash_led(20);
-	}
-}
-*/
-ISR(SPI_STC_vect)
+ISR(SPI_STC_vect) // spi per interrupt for higher performance
 {
 	SPDR = spi_buffer[++spi_buffer_index];
 	spi_buffer[spi_buffer_index] = SPDR;
 	
-	/*
-	incoming[received++] = received_from_spi(0x00);
-	if (received >= BUFSIZE || incoming[received-1] == 0x00) {
-		parse_message();
-		received = 0;
-	}
-	*/
 }
 
 ISR (TIMER1_COMPA_vect)
 {
-	//reset
+	// reset timer1
 	add_task(clock_inc);
 }
 
@@ -213,11 +167,17 @@ ISR (TIMER1_COMPB_vect)
 
 int main(void)
 {
-	add_task(init);//run init with scheduler
-	main_loop://While(1){//Contexswitch means stack mark and goto not
+	add_task(init); // run init with scheduler
+	main_loop: // While(1){
+		// Contexswitch means stack mark and goto not
 		scheduler[schedule_first]();
 		scheduler[schedule_first]=default_schedule;
-		//if(schedule_first!=schedule_last) schedule_inc(schedule_first);//looks better but doesnt work with schedule_max tasks because the task sticks
-		(schedule_first==schedule_last)?schedule_first=schedule_inc(schedule_last):schedule_inc(schedule_first);//schedule_max tasks works as long as now new task will be added
+		
+		// if(schedule_first!=schedule_last)
+		//	schedule_inc(schedule_first);//looks better but doesnt work with schedule_max tasks because the task sticks
+		
+		(schedule_first==schedule_last)?
+			schedule_first=schedule_inc(schedule_last) : 
+			schedule_inc(schedule_first); // schedule_max tasks works as long as now new task will be added
 	goto main_loop;//}
 }
