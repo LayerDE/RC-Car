34c967a0d8cb49ec16e639e5e0b05f94af3859e5
LayerDE <the_gamer@gmx.net>
2017-11-05 01:08:45 +0100

    add 5V ICs

---
+++ IC RGB-LED/main.c
@@ -0,0 +1,18 @@
+/*
+ * IC RGB-LED.c
+ *
+ * Created: 05.11.2017 01:07:14
+ * Author : Victor
+ */ 
+
+#include <avr/io.h>
+
+
+int main(void)
+{
+    /* Replace with your application code */
+    while (1) 
+    {
+    }
+}
+
