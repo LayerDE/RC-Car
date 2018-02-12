1d3bef206694ecb858005e2b302c9ee10916025f
LayerDE <the_gamer@gmx.net>
2018-02-12 01:26:35 +0100

    added axe_isr and null functions

--- joystick_in.c
+++ joystick_in.c
@@ -2,13 +2,16 @@
 #include <stdio.h>
 #include <linux/joystick.h>
 #include <stdlib.h>
+
 typedef int (*calc_axe_ptr)(__s16);
+typedef void (*axe_isr_ptr)(int);
 typedef void (*btn_isr_ptr)(bool);
 calc_axe_ptr calc_axes[];
-btn_isr_ptr btn_fncs[]; //executed with a button event
+axe_isr_ptr axe_functions[];
+btn_isr_ptr btn_functions[]; //executed with a button event
 int axes[];
 bool buttons[];
-int js_handle
+int js_handle;
 __u32 last_time;
 __u32 time_last_event;
 size_t get_axis_count(int fd)
@@ -29,6 +32,12 @@ int calc_axe_1m(__s16 raw){
 }
 int calc_axe_1p(__s16 raw){
     return -1<<(sizeof(__s16)*8-1)+raw;
+}
+void btn_null_exec(bool state){
+
+}
+void axe_null_exec(int value){
+
 }
 
 size_t get_button_count(int fd)
@@ -59,6 +68,17 @@ void init(char js_device[]){
     axes = malloc(sizeof(int)*get_axis_count(js_handle));
     calc_axes = malloc(sizeof(calc_axe_ptr)*get_axis_count(js_handle));
     buttons = malloc(sizeof(bool)*get_button_count(js_handle));
+    axe_functions = malloc(sizeof(axe_isr_ptr)*get_axis_count(js_handle));
+    btn_functions = malloc(sizeof(btn_isr_ptr)*get_button_count(js_handle));
+    for(x = 0;x<get_axis_count(js_handle);x++){
+        axes[x] = 0;
+        calc_axes = calc_axe_0mp;
+        axe_functions = axe_null_exec;
+    }
+    for(x = 0;x<get_button_count(js_handle);x++){
+        buttons = false;
+        btn_functions = btn_null_exec;
+    }
     char name[128];
 	if (ioctl(fd, JSIOCGNAME(sizeof(name)), name) < 0);
 		strncpy(name, "Unknown", sizeof(name));
@@ -70,10 +90,10 @@ void input_thread(){
         switch (event.type)
         {
             case JS_EVENT_BUTTON:
-                buttons[event.number] = event.value ? true : false;
+                btn_functions[event.number](buttons[event.number] = event.value ? true : false);
                 break;
             case JS_EVENT_AXIS:
-                axes[event.number] = calc_axes[event.number](event.value);
+                axe_functions[event.number](axes[event.number] = calc_axes[event.number](event.value));
                 break;
             case JS_EVENT_INIT:
                 break;
